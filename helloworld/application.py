e1b98bd32bc64c3b93e978546350acf1926bb575
EC2 Default User <ec2-user@ip-172-31-80-43.ec2.internal>
2023-07-30 21:59:00 +0000

    project-ver_06

--- helloworld/application.py
+++ helloworld/application.py
@@ -20,40 +20,11 @@ def post():
 
 
 
-# S3 - UPLOAD IMAGE
-@application.route('/upload_image', methods=['GET'])
+@application.route('/upload_image', methods=['POST'])
 def upload_image():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
-'''
-    bucket = 'jce-cloud-project'
-    img = request.files['img']
-    s3 = boto3.resource('s3', region_name='us-east-1')
-    img_path  = "images/%s.jpg" %  (str(uuid.uuid4()))
-    s3.Bucket(bucket).upload_fileobj(img, img_path, ExtraArgs={'ACL': 'public-read', 'ContentType': 'image/jpeg'}) 
-    img_url = 'https://jce-cloud-project.s3.amazonaws.com/'+ img_path
     
-    
-    rekognition = boto3.client("rekognition", region_name = 'us-east-1')
-    
-    key = img_path
-
-    response = rekognition.detect_faces(
-    Image={
-        'S3Object': {
-            'Bucket': bucket,
-            'Name': key,
-        }
-    }
-    )
-    
-    
-    print(response['FaceDetails'][0]['Confidence'])
-    confidence = response['FaceDetails'][0]['Confidence'];
-    sharpness = response['FaceDetails'][0]['Quality']['Sharpness'];
 
 
-    return {"img_url": img_url, "confidence": confidence, "sharpness": sharpness}
-    '''
-    
-    if __name__ == '__main__':
+if __name__ == '__main__':
     flaskrun(application)
