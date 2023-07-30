12cd095e56a2c41b9d2a65862f4ff52c53a75ce6
EC2 Default User <ec2-user@ip-172-31-80-43.ec2.internal>
2023-07-30 22:04:30 +0000

    project-ver_07

--- helloworld/application.py
+++ helloworld/application.py
@@ -22,7 +22,34 @@ def post():
 
 @application.route('/upload_image', methods=['POST'])
 def upload_image():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    bucket = 'jce-cloud-project'
+    img = request.files['img']
+    s3 = boto3.resource('s3', region_name='us-east-1')
+    img_path  = "images/%s.jpg" %  (str(uuid.uuid4()))
+    s3.Bucket(bucket).upload_fileobj(img, img_path, ExtraArgs={'ACL': 'public-read', 'ContentType': 'image/jpeg'}) 
+    img_url = 'https://jce-cloud-project.s3.amazonaws.com/'+ img_path
+    
+    
+    rekognition = boto3.client("rekognition", region_name = 'us-east-1')
+    
+    key = img_path
+
+    response = rekognition.detect_faces(
+    Image={
+        'S3Object': {
+            'Bucket': bucket,
+            'Name': key,
+        }
+    }
+    )
+    
+    
+    print(response['FaceDetails'][0]['Confidence'])
+    confidence = response['FaceDetails'][0]['Confidence'];
+    sharpness = response['FaceDetails'][0]['Quality']['Sharpness'];
+
+
+    return {"img_url": img_url, "confidence": confidence, "sharpness": sharpness}
     
 
 
